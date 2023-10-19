d548c38f17b045abaf9f3ccf5664d186291df348
Faisal Alware <85944127+faisalalware@users.noreply.github.com>
2023-10-19 11:30:44 +0530

    added BMR & AMR values in table form

--- main.js
+++ main.js
@@ -10,20 +10,45 @@ form.addEventListener('submit', function (e) {
   // This method prevents form from submiting
   e.preventDefault();
 
-  // Get form values and elements from the document
+  // Get input values from form
   const nameVal = document.querySelector('#name').value;
-  const age = document.querySelector('#age').value;
+  const age = parseInt(document.querySelector('#age').value);
   const height = parseInt(document.querySelector('#height').value);
   const weight = parseInt(document.querySelector('#weight').value);
+
+  //Get elements from the document
   const results = document.querySelector('#results');
   const status = document.querySelector('#status');
   const idealWeight = document.querySelector('#ideal-weight');
+  const maintainWeightElement = document.querySelector('#maintain-weight');
+  const mildWeightLossElement = document.querySelector('#mild-weight-loss');
+  const moderateWeightLossElement = document.querySelector('#moderate-weight-loss');
+  const extremeWeightLossElement = document.querySelector('#extreme-weight-loss');
+  const sedentaryElement = document.querySelector('#sedentary');
+  const lightlyActiveElement = document.querySelector('#lightly-active');
+  const moderatelyActiveElement = document.querySelector('#moderately-active');
+  const activeElement = document.querySelector('#active');
+  const veryActiveElement = document.querySelector('#very-active');
 
   // Calculate BMI as per Weight & Height
   const bmi = (weight / ((height * height) / 10000)).toFixed(2);
 
   // Calculate Ideal Weight as per Height
   const iw = (22 * ((height * height) / 10000)).toFixed(0);
+
+  // Calculate BMR as per Weight, Height & Age
+  const bmr = (66.47 + (13.75 * weight) + (5.003 * height) - (6.755 * age)).toFixed(0);
+  const maintainWeight = (66.47 + (13.75 * weight) + (5.003 * height) - (6.755 * age) + 250).toFixed(0);
+  const moderateWeightLoss = (66.47 + (13.75 * weight) + (5.003 * height) - (6.755 * age) - 250).toFixed(0);
+  const extremeWeightLoss = (66.47 + (13.75 * weight) + (5.003 * height) - (6.755 * age) - 500).toFixed(0);
+
+  //Calculate AMR as per BMR
+  const sedentary = (bmr * 1.2).toFixed(0);
+  const lightlyActive = (bmr * 1.375).toFixed(0);
+  const moderatelyActive = (bmr * 1.55).toFixed(0);
+  const active = (bmr * 1.725).toFixed(0);
+  const veryActive = (bmr * 1.9).toFixed(0);
+  console.log(veryActive);
 
   // Form validation  
   if (nameVal === '') {
@@ -40,8 +65,21 @@ form.addEventListener('submit', function (e) {
     results.innerHTML = `Please give me valid weight ${weight}`;
   }
   else {
-    //Print BMI value
+    // Print BMI value
     results.innerHTML = `${nameVal} your score is ${bmi}`;
+
+    // Print BMR value
+    maintainWeightElement.innerHTML = `Consume ${maintainWeight} calories per day`;
+    mildWeightLossElement.innerHTML = `Consume ${bmr} calories per day`;
+    moderateWeightLossElement.innerHTML = `Consume ${moderateWeightLoss} calories per day`;
+    extremeWeightLossElement.innerHTML = `Consume ${extremeWeightLoss} calories per day`;
+
+    //Print AMR Value
+    sedentaryElement.innerHTML = `${sedentary} calories per day`;
+    lightlyActiveElement.innerHTML = `${lightlyActive} calories per day`;
+    moderatelyActiveElement.innerHTML = `${moderatelyActive} calories per day`;
+    activeElement.innerHTML = `${active} calories per day`;
+    veryActiveElement.innerHTML = `${veryActive} calories per day`;
 
     // Print Status & Ideal Weight as per BMI & Height
     if (bmi < 16) {
