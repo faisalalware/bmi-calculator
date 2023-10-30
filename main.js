18513095dce70e5fd2500a0efb710e0c763ced6a
Faisal Alware <85944127+faisalalware@users.noreply.github.com>
2023-10-30 15:42:52 +0530

    form validation code changes

--- main.js
+++ main.js
@@ -44,66 +44,89 @@ form.addEventListener('submit', function (e) {
   const active = (bmr * 1.725).toFixed(0);
   const veryActive = (bmr * 1.9).toFixed(0);
 
-  // Form validation  
-  if (name === '') {
+  // Form validation 
+  if (name === '' || age === '' || height === '' || weight === '') {
+    alert("Please enter all fields");
+    e.preventDefault();
+  }
+  else if (name === '') {
     alert("Please enter name");
     e.preventDefault();
   }
-  else if (age === '' || isNaN(age)) {
+  else if (age === '') {
     alert("Please enter age");
     e.preventDefault();
   }
-  else if (height === '' || isNaN(height)) {
+  else if (isNaN(age)) {
+    alert("Please enter number for age");
+    e.preventDefault();
+  }
+  else if (age < 18 || age > 100) {
+    alert("Please enter valid age");
+    e.preventDefault();
+  }
+  else if (height === '') {
     alert("Please enter height");
     e.preventDefault();
   }
-  else if (weight === '' || isNaN(weight)) {
+  else if (isNaN(height)) {
+    alert("Please enter number for height");
+    e.preventDefault();
+  }
+  else if (height < 150 || height > 180) {
+    alert("Please enter valid height");
+    e.preventDefault();
+  }
+  else if (weight === '') {
     alert("Please enter weight");
+    e.preventDefault();
+  }
+  else if (isNaN(weight)) {
+    alert("Please enter number for weight");
+    e.preventDefault();
+  }
+  else if (weight < 50 || weight > 150) {
+    alert("Please enter valid weight");
     e.preventDefault();
   }
   else {
     // Print BMI value
     bmiScoreElement.innerHTML = `${name} your score is ${bmi}`;
 
-    // Print Status & Ideal Weight as per BMI value
+    // Print Status as per BMI value
     if (bmi < 16) {
       statusElement.innerHTML = `at the age of ${age} you are Severely Thin`;
-      idealWeightElement.innerHTML = `Maintain your weight upto ${iw} kg`;
     }
     else if (bmi > 16 && bmi < 17) {
       statusElement.innerHTML = `at the age of ${age} your are Moderately Thin`;
-      idealWeightElement.innerHTML = `Maintain your weight upto ${iw} kg`;
     }
     else if (bmi > 17 && bmi < 18.5) {
       statusElement.innerHTML = `at the age of ${age} you are Mildly Thin`;
-      idealWeightElement.innerHTML = `Maintain your weight upto ${iw} kg`;
     }
     else if (bmi > 18.5 && bmi < 25) {
       statusElement.innerHTML = `at the age of ${age} you are in Normal Weight`;
-      idealWeightElement.innerHTML = `Maintain your weight upto ${iw} kg`;
     }
     else if (bmi > 25 && bmi < 30) {
       statusElement.innerHTML = `at the age of ${age} you are Overweight`;
-      idealWeightElement.innerHTML = `Maintain your weight upto ${iw} kg`;
     }
     else if (bmi > 30 && bmi < 35) {
       statusElement.innerHTML = `at the age of ${age} you are Mildly Obese`;
-      idealWeightElement.innerHTML = `Maintain your weight upto ${iw} kg`;
     }
     else if (bmi > 35 && bmi < 40) {
       statusElement.innerHTML = `at the age of ${age} you are Moderately Obese`;
-      idealWeightElement.innerHTML = `Maintain your weight upto ${iw} kg`;
     }
     else if (bmi > 40) {
       statusElement.innerHTML = `at the age of ${age} you are Severely Obese`;
-      idealWeightElement.innerHTML = `Maintain at least ${iw} kg`;
     }
 
+    // Print Ideal Weight
+    idealWeightElement.innerHTML = `maintain your weight upto ${iw} kg`;
+
     // Print BMR value
-    maintainWeightElement.innerHTML = `Consume ${maintainWeight} calories per day`;
-    mildWeightLossElement.innerHTML = `Consume ${bmr} calories per day`;
-    moderateWeightLossElement.innerHTML = `Consume ${moderateWeightLoss} calories per day`;
-    extremeWeightLossElement.innerHTML = `Consume ${extremeWeightLoss} calories per day`;
+    maintainWeightElement.innerHTML = `consume ${maintainWeight} calories per day`;
+    mildWeightLossElement.innerHTML = `consume ${bmr} calories per day`;
+    moderateWeightLossElement.innerHTML = `consume ${moderateWeightLoss} calories per day`;
+    extremeWeightLossElement.innerHTML = `consume ${extremeWeightLoss} calories per day`;
 
     //Print AMR value
     sedentaryElement.innerHTML = `${sedentary} calories per day`;
